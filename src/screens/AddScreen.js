791d7360df0896fe3dbf17920a805c3686544424
Romain Ricord <romain.ricord.pro@gmail.com>
2022-05-19 15:21:58 +0200

    Rajout d'une API

--- src/screens/AddScreen.js
+++ src/screens/AddScreen.js
@@ -1,6 +1,15 @@
 import {StyleSheet,View,Text,ImageBackground} from 'react-native'
-import { TextInput,Button } from 'react-native-paper'
+import { TextInput,Button,DefaultTheme } from 'react-native-paper'
 import {useState} from 'react'
+import axios from 'axios'
+
+const theme = {
+  colors: {
+    text: '#7a34eb',
+    placeholder: '#7a34eb', 
+    primary: '#7a34eb'
+  },
+};
 
 
 const AddScreen = () => {
@@ -18,36 +27,66 @@ const AddScreen = () => {
                 <Text style={{color:'white',fontWeight:'bold',fontSize:32,marginTop:20,marginBottom:20,textDecorationLine:'underline',textAlign:'center'}}>Ajouter une activité</Text>
                 
                 <TextInput
+                    theme={theme}
                     style={styles.TextInput}
                     label="Date (Jour mois année)"
                     value={date_}
                     onChangeText={setDate_}
+                    
                 />
                 <TextInput
+                    theme={theme}
                     style={styles.TextInput}
                     label="Titre"
                     value={title_}
                     onChangeText={setTitle_}
                 />
                 <TextInput
+                    theme={theme}
                     style={styles.TextInput}
                     label="Distance ( en KM )"
                     value={distance_}
                     onChangeText={setDistance_}
                 />
                 <TextInput
+                    theme={theme}
                     style={styles.TextInput}
                     label="Durée"
                     value={duration_}
                     onChangeText={setDuration_}
                 />
                 <TextInput
+                    theme={theme}
                     style={styles.TextInput}
                     label="Image (URL)"
                     value={image_}
                     onChangeText={setImage_}
                 />
-                <Button style={{margin:20,borderRadius:20}} mode="contained" onPress={() => console.log('Pressed')}>
+                <Button style={{margin:20,borderRadius:20}} 
+                mode="contained" 
+                onPress={() => {
+                    console.log('Pressed')
+                    const testconfigheaders = {
+                        headers: {
+                          'Content-Type': 'application/json;charset=UTF-8',
+                          "Access-Control-Allow-Origin": "*",
+                          "date":date_,
+                          "title":title_,
+                          "distance":distance_,
+                          "duration":duration_,
+                          "image":image_
+                        }
+                      }
+                    
+                    
+                      axios.post('http://192.168.1.16:3000/card',{},testconfigheaders)
+                      .then((res) =>{
+                        console.log(res)
+                      })
+                      .catch((err) => {
+                        console.log(err)
+                      })
+                }}>
                     Valider
                 </Button>
             </ImageBackground>
@@ -59,7 +98,11 @@ const styles = StyleSheet.create({
     TextInput:{
         marginLeft:40,
         marginRight:40,
-        marginBottom:20
+        marginBottom:20,
+        backgroundColor:'rgba(0,0,0,0.8)',
+        color:'#7a34eb',
+        fontWeight:'bold',
+        fontSize:24
     }
 });
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,36 @@
 import { Text,View,ScrollView,ImageBackground } from 'react-native';
 import CardComponent from '../components/CardComponent';
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 
 const data = require("../../assets/data.json")
 
 const HomeScreen = () => {
+
+    const [primarydata_,setPrimarydata] = useState([])
+
+    console.log(primarydata_)
+
+    useEffect(() => {
+        fetch('http://192.168.1.16:3000/')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            console.log("Récupération data")
+            setPrimarydata(responseJson)
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+        
+  
+    },[])
+
     return(
         <View style={{flex:1}}>
             <ImageBackground style={{flex:1,justifyContent:'center'}} resizeMode='cover' source={{uri:'https://images.hdqwalls.com/wallpapers/asia-neon-city-lights-reflections-01.jpg'}}>
                 <ScrollView style={{flex:1}}>
                     <Text style={{color:'white',fontWeight:'bold',fontSize:32,marginTop:20,marginBottom:20,textDecorationLine:'underline',textAlign:'center'}}>Home</Text>
-                    {data.map((e,k) => {
+                    {primarydata_.map((e,k) => {
 
                     const {title,date,distance,duration,picture} = e
 
